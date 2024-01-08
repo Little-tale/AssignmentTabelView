591087e58616209647270fdb0a23cdc81e7900bf
judge_happily <you3192001@gmail.com>
2024-01-08 21:26:25 +0900

    새롭게 쇼핑 리스트 테이블 뷰를 생성

---
+++ AssignmentTabelView/ShoppingListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ShoppingListTableViewCell.swift
+//  AssignmentTabelView
+//
+//  Created by Jae hyung Kim on 1/8/24.
+//
+
+import UIKit
+
+class ShoppingListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
